918dabe7ced5fb9327535a4562162226b8078f90
Mahmoud Akram <mahmoudakrm20@gmail.com>
2025-06-10 07:21:38 +0300

    Fixing Component Name to deploy on vercel

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "../components/Sidebar";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
